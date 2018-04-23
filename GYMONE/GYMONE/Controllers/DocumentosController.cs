8b455b9c8b50864726a0a36613db28b337c92c83
PSLCOL\mrivast <mrivast@psl.com.co>
2018-04-23 13:58:40 -0500

    Project

---
+++ GYMONE/GYMONE/Controllers/DocumentosController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GYMONE.Controllers
+{
+    public class DocumentosController : Controller
+    {
+        //
+        // GET: /Documentos/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
